79f3ce20c64fc5734b72a6809dc46a6cb99e92e4
Allison Weins <3174849+2percentsilk@users.noreply.github.com>
2020-12-07 14:45:55 -0800

    Update postCreate.sh

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,6 +1,3 @@
 cd backend
 pip install -r requirements/local.txt
 docker-compose -f ../services.yml up --d
-python manage.py migrate
-echo "Ac0mPl3xP@55w0rd" | python manage.py createsuperuser --username admin --email a@a.com --noinput
-python manage.py loaddata avatars
